cd635f66d77c137d216ceb8821de34e3f0d5ddad
Yiğit Can Başalma <yigit.basalma@local>
2023-06-01 13:33:54 +0300

    Fix

--- vars/lib_codeQualityTestController.groovy
+++ vars/lib_codeQualityTestController.groovy
@@ -1,13 +1,11 @@
 def call(Map config) {
-    withMaven(maven: config.b_config.project.builderVersion) {
-        withSonarQubeEnv(config.sonarqube_env_name) {
-            sh """
-            ${config.sonarqube_home}/bin/sonar-scanner \
-            -Dsonar.language=js \
-            -Dsonar.projectKey=${config.sonar_qube_project_key} \
-            -Dsonar.sources=.
-            """
-        }
+    withSonarQubeEnv(config.sonarqube_env_name) {
+        sh """
+        ${config.sonarqube_home}/bin/sonar-scanner \
+        -Dsonar.language=js \
+        -Dsonar.projectKey=${config.sonar_qube_project_key} \
+        -Dsonar.sources=.
+        """
     }
 
     timeout(time: 1, unit: 'HOURS') {
