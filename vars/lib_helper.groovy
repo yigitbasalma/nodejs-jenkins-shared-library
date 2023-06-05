5074d80b40eb20f8fac31ed13b2c6543b70a9ab8
Yiğit Can Başalma <yigit.basalma@local>
2023-06-05 15:46:11 +0300

    Fix

--- vars/lib_helper.groovy
+++ vars/lib_helper.groovy
@@ -47,6 +47,10 @@ def preBuildConfigurations(Map config) {
 }
 
 def triggerJob(Map config) {
+    if ( ! config.sequential_deployment_mapping.containsKey(config.job_name) ) {
+        return
+    }
+
     next_job_name = config.sequential_deployment_mapping[config.job_name]
     build job: "${config.job_base}/${next_job_name}", propagate: false, wait: false, parameters: [string(name: 'IMAGE', value: config.b_config.imageTag)]
 }
