f731459042108f7f6bdf10536bd36bf04647732d
Yiğit Can Başalma <yigit.basalma@local>
2023-09-19 16:34:31 +0300

    Critical changes

--- vars/lib_buildController.groovy
+++ vars/lib_buildController.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
     sh """
     npm install && \
-    npm run ${config.b_config.project.buildCommand}
+    npm run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
     """
 }
