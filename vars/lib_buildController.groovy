09319303480002191d6f5ae45740e0d6c9b6581a
Yiğit Can Başalma <yigit.basalma@local>
2023-09-24 17:21:37 +0300

    Fix

--- vars/lib_buildController.groovy
+++ vars/lib_buildController.groovy
@@ -1,6 +1,12 @@
 def call(Map config) {
+    def builder = "npm"
+
+    if ( config.b_config.project.builderVersion != "nodejs" ) {
+        builder = tool config.b_config.project.builderVersion
+    }
+
     sh """
-    npm install && \
-    npm run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
+    ${builder} install && \
+    ${builder} run ${config.b_config.project.buildCommand ? config.b_config.project.buildCommand : 'build'}
     """
 }
